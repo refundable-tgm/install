240b1dc32a27468428606732d24e0eed2a809a36
Michael Beier <admin@mbeier.at>
2020-12-27 21:19:49 +0100

    final bugfixes

--- images/mongo/init-db.sh
+++ images/mongo/init-db.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-root_username=$(<"$MONGO_INITDB_ROOT_USERNAME_FILE")
-root_password=$(<"$MONGO_INITDB_ROOT_PASSWORD_FILE")
-root_db=$MONGO_INITDB_DATABASE
-user_username=$(<"$MONGO_USERNAME_FILE")
-user_password=$(<"$MONGO_PASSWORD_FILE")
-new_db=$MONGO_DATABASE
+mongo -- "$MONGO_INITDB_DATABASE" << EOF
+    var rootUser = '$(cat "$MONGO_INITDB_ROOT_USERNAME_FILE")';
+    var rootPassword = '$(cat "$MONGO_INITDB_ROOT_PASSWORD_FILE")';
+    var admin = db.getSiblingDB('admin');
+    admin.auth(rootUser, rootPassword);
 
-mongo -- root_db << EOF
-db.auth(root_username, root_password);
-var sib = db.getSiblingDB(new_db);
-sib.createUser({user: user_username, pwd: user_password, roles: [{role: 'readWrite', db: new_db}]};
+    var user = '$(cat "$MONGO_USERNAME_FILE")';
+    var passwd = '$(cat "$MONGO_PASSWORD_FILE")';
+    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
 EOF
