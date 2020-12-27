1dde4bef9aa87539d4cc57398038a12274e80fd7
Michael Beier <admin@mbeier.at>
2020-12-27 20:15:25 +0100

    more bugfixes

--- images/mongo/init-db.sh
+++ images/mongo/init-db.sh
@@ -3,8 +3,8 @@
 root_username=$(<"$MONGO_INITDB_ROOT_USERNAME_FILE")
 root_password=$(<"$MONGO_INITDB_ROOT_PASSWORD_FILE")
 root_db=$MONGO_INITDB_DATABASE
-user_username=$(<"MONGO_USERNAME_FILE")
-user_password=$(<"MONGO_PASSWORD_FILE")
+user_username=$(<"$MONGO_USERNAME_FILE")
+user_password=$(<"$MONGO_PASSWORD_FILE")
 new_db=$MONGO_DATABASE
 
 mongo -- root_db << EOF
